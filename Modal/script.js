e428e6fa9b101d89cc8822f0fb1e5a68f13aab63
codewithgulzar <muhammad.gulzar8991@gmail.com>
2023-10-26 09:59:48 +0530

    create model project

--- Modal/script.js
+++ Modal/script.js
@@ -1,6 +1,7 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
 
 modalBtn.addEventListener("click", () => {
     modal.classList.add("open-modal");
@@ -8,4 +9,15 @@ modalBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
     modal.classList.remove("open-modal");
+});
+
+// modalOverlay.addEventListener("click", () => {
+//     modal.classList.remove("open-modal");
+// });
+
+modalOverlay.addEventListener("click", (event) => {
+    console.log(event);
+    if (event.target === modalOverlay) {
+        modal.classList.remove("open-modal");
+    }
 });
